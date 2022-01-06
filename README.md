635f417c851054b520051fa19f14105afe20009f
joe_yang <yo2man0929@gmail.com>
2022-01-06 10:54:56 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Jenkins in Kubernetes
+This repository is a POC for setting up a simple HA Jenkins  running in Kubernetes by using kind.
+
+**Prerequisite**
+`
+Please install helm, kind, docker first
+
+`
+
+# The repo is running jenkins in k8s by using kind
+
+
+### For HA purpose in reality, please use 2 more replicas and pod antiaffinity to run in different nodes. Please refer to the jenkins-tailored-values.yaml 
+
+### Given the conditions of using persist volume in cloud, it is better to some GCP storageClass in PVC.
+https://kubernetes.io/docs/concepts/storage/storage-classes/
+
+##  how to run the POC
+
+`
+bash -x bootstrap.sh`
+
+``
+
+
+
---
+++ bootstrap.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+set -euo pipefail
+
+# set up kind cluster
+kind create cluster --wait 10m --config kind-config/kind-ingress-config.yaml
+
+
+# install ingress-nginx
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s
+
+# install app-ingress
+kubectl apply -f app/app-ingress.yaml -n default
+kubectl apply -f app/ingress-nginx-deployment.yaml -n ingress-nginx
+
+
+helm upgrade --install jenkins ./jenkins-k8s -f jenkins-tailored-values.yaml -n default
+
+# delete ingress-nginx webhook for mis-dection
+kubectl delete   validatingwebhookconfigurations ingress-nginx-admission -A
+
+# apply ingress yaml manually
+kubectl apply -f app/app-ingress.yaml
+
+echo "Use browser to connect 127.0.0.1:80"
+
